7aee416c4fba88aae7656b789582e0c634cfeba3
godtanatos <igarcharvinsky@gmail.com>
2021-03-15 01:45:31 +0300

    answer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if(arr == null || arr.length==0){
+    return 0;}
+     var min = arr[0];
+     for(var i=1; i<arr.length; i++){
+       if(arr[i] < min){
+         min = arr[i];   
+       }
+      }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if(arr == null || arr.length==0){
+    return 0}
+    var min = arr[0]
+     var max = arr[0];
+     for(var i=1; i<arr.length; i++){
+       if(arr[i] > max){
+         max = arr[i];   
+       }
+      }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if(arr == null || arr.length==0){
+    return 0}
+     var sum = 0;
+     for(var i=0; i<arr.length; i++){
+       if(i!= arr.length){
+         sum += arr[i];  
+       }
+      }
+    return sum/arr.length;
 }
